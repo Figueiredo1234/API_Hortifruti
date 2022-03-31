2a34ca029aa7142055f78be4dc9bc0aaaca1e2ec
Figueiredo1234 <86685363+Figueiredo1234@users.noreply.github.com>
2022-03-31 17:22:56 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','victorpfsilva.mysql.database.azure.com');
-define('DB_USER','victorpfsilva@victorpfsilva');
-define('DB_PASS','salmos27#');
-define('DB_NAME','hortfruitbd');
+define ('DB_HOST','localhost');
+define ('DB_USER','victorpfsilva@victorpfsilva');
+define ('DB_PASS','salmos27#');
+define ('DB_NAME','lojabolos_tb');
