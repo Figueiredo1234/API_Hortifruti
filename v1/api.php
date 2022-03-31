fbe60aed6ea4336f2b6fd236a7c5cac9d2855f64
Figueiredo1234 <86685363+Figueiredo1234@users.noreply.github.com>
2022-03-31 17:25:25 -0300

    Update api.php

--- v1/api.php
+++ v1/api.php
@@ -1,105 +1,105 @@
 <?php
 
-require_once '../model/Operacao.php';
+require_once('../model/operacao.php');
 
 function isTheseParametersAvailable($params){
-$avalilable = true;
-$missingparams = "";
+     $available = true;
+     $missingparams = " ";
 
-foreach($params as $param){
-    if(!isset($_POST[$param]) || strlen($_POST[$param])<=0){
-        $avalilable = false;
-        $missingparams = $missingparams.", ".$param;
-    }
-}
+    foreach($params as $param){
+    if(!isset($_POST[$param]) || strlen ($_POST[$param])<=0){
+    $availilable = false;
+    $missingparams = $missingparams. ", ".$param;
 
-if(!$avalilable){
-    $response = array();
-    $response['error'] = true;
-    $response['message'] = 'Parameters '.substr($missingparams, 1,strlen($missingparams)).' missing';
+         }
+     }
+
+    if(!$available){
+     $response = array();
+     $response['error'] = true;
+     $response['message'] = 'parameters' .substr($missingparams, 1,
+     strlen($missingparams)).'missing';
 
     echo json_encode($response);
 
-    die();
+         die();
 }
 }
 
 $response = array();
 
 if(isset($_GET['apicall'])){
+
     switch($_GET['apicall']){
 
-        case 'createFruta':
-           isTheseParametersAvailable(array('campo_2','campo_3','campo_4'));
+case 'create':
+    isTheseParametersAvailable(array('campo_2', 'campo_3', 'campo_4'));
+    $db = new Operacao();
+    $result = $db->createBolos(
+    $_POST['campo_2'],
+    $_POST['campo_3'],
+    $_POST['campo_4'],
 
-            $db = new Operacao();
-
-            $result = $db->createFruta(
-                $_POST['campo_2'],
-                $_POST['campo_3'],
-                $_POST['campo_4'],
-               
-            );
-
-            if($result){
-                $response['error'] = false;
-                $response['message']= 'Dados inseridos com sucesso.';
-                $response['dadoscreate'] = $db->getFrutas();
-            }else{
-                $response['error'] = true;
-                $response['message']= 'Dados nao foram inseridos.';
-            }
-
-        break;
-        case 'getFrutas':
-            $db= new Operacao();
-            $response['error'] = false;
-            $response['message']= 'Dados Listados com sucesso.';
-            $response['dadoslista']=$db->getFrutas();
-
-        break;
-        case 'updateFrutas':
-            isTheseParametersAvailable(array('campo_1','campo_2','campo_3','campo_4'));
-
-            $db = new Operacao();
-            $result = $db->updateFrutas(
-                $_POST['campo_1'],
-                $_POST['campo_2'],
-                $_POST['campo_3'],
-                $_POST['campo_4']
-            );
-
-            if($result){
-                $response['error'] = false;
-                $response['message'] = "Dados alterados com sucesso.";
-                $response['dadosalterar'] = $db->getFrutas();
-            }else{
-                $response['error'] = true;
-                $response['message'] = "Dados não alterados.";
-            }
-        break;
-        case 'deleteFrutas':
-            if(isset($_GET['uid'])){
-                $db = new Operacao();
-                if($db->deleteFrutas($_GET['uid'])){
-                    $response['error'] = false;
-                    $response['message'] = "Dado excluido com sucesso";
-                    $response['deleteFrutas'] = $db->getFrutas();
-
-                }else{
-                    $response['error'] = true;
-                    $response['message'] = "Algo deu errado";
-                }
-            }else{
-                $response['error'] = true;
-                $response['message'] = "Dados não apagados.";
-            }
-        break;
-
-    }
+);
+    if($result){
+    $response['error'] -false;
+    $response['message'] = 'dados inseridos com sucesso.';
+    $response['dadoscreate'] =
+    $db->getBolos();
 }else{
+    $response['error'] = true;
+    $response['message']= 'Dados nao foram inseridos';
+
+}
+    break;
+    case 'getBolos':
+    $db= new Operacao();
+    $response['error'] = false;
+    $response['message']= 'dados Listados com sucesso.';
+    $response['dadoslista']=$db->getBolos();
+
+break;
+    case 'updateBolos':
+    isTheseParametersAvailable(array('campo_1', 'campo_2', 'campo_3','campo_4'));
+    $db = new Operacao();
+    $result = $db->updateBolos(
+    $_POST['campo_1'],
+    $_POST['campo_2'],
+    $_POST['campo_3'],
+    $_POST['campo_4'],
+);
+
+    if($result){
+     $response['error'] = false;
+     $response['message'] = "Dados alterados com sucesso.";
+     $response['dadosalterar'] =
+     $db ->getBolos();
+}else{
+    $respopnse['error'] = true;
+    $response['message'] = "Dados não alterados";
+}
+    break;
+    case 'deleteBolos':
+    if(isset($_GET['uid'])){
+    $db = new Operacao();
+    if($db->deleteBolos($_GET['uid'])){
+    $response['error'] = false;
+    $response['message'] = "Dado excluido com sucesso";
+    $response['deleteFrutas'] = $db->getBolos();
+
+      }else{
+    $response['error'] = true;
+    $response['message'] = "Algo deu errado";
+      }
+      }else{
+    $response['error'] = true;
+    $response['message'] = "Dados não apagados";
+}
+      break;
+    }
+      }else{
     $response['error'] = true;
     $response['message'] = "Chamada de Api com defeito";
 }
 
-echo json_encode($response);
+    echo json_encode($response);
